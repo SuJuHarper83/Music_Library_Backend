06fd0bb5bfa5dd5d8b3d3cbaf1caf4d0a8146c1d
Your Name <you@example.com>
2022-11-14 16:50:47 -0700

    Created Serializer file

--- music_library_backend/urls.py
+++ music_library_backend/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from music import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/music/', include ('music.urls')),
+    path('api/music/<int:pk>/')
 ]
